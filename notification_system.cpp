59b29a345fef5892c93fdfad772c1b4782f639f4
AnuragSinghDhami <your-email@example.com>
2026-08-13 18:00:17 +0530

    added factory pattern

---
+++ notification_system.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+enum class NotificationType{
+    EMAIL, SMS, PUSH
+};
+
+class Notification{
+
+public:
+    virtual ~Notification() = default;
+    virtual void send(const string& message) const = 0;
+};
+
+class Email : public Notification{
+
+public :
+
+    void send(const string& message) const override{
+        cout << "sending email : " << message << endl;
+    }
+};
+
+class sms : public Notification{
+
+public :
+
+    void send(const string& message) const override{
+        cout << "sending sms : " << message << endl;
+    }
+};
+
+class NotificationFactory{
+
+public:
+
+    static unique_ptr<Notification> createNotification(NotificationType type){
+
+        switch(type){
+            case NotificationType::EMAIL :
+                return make_unique<Email>();
+            case NotificationType::SMS :
+                return make_unique<Email>();
+        }
+    }
+};
+
+int main(){
+
+    try{
+        unique_ptr<Notification> email = NotificationFactory::createNotification(NotificationType::EMAIL);
+        email->send("your order has been shipped.");
+    }
+    catch(const exception& e){
+        cerr << "error" << e.what() << endl;
+    }
+}
